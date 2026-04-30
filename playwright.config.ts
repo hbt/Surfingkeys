88ac337be98719e99d221b8622efdb589e5a0129
hbt <hassenbentanfous@gmail.com>
2026-04-30 21:36:59 +0800

    speed up tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
     testDir: './tests/playwright',
     workers: 1,
+    retries: 2,
     timeout: 30_000,
     use: {
         trace: 'off',
